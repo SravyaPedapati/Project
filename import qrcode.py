9f13b7f77c80fa928a7bb72b2dcca3c054403487
SravyaPedapati <164610041+SravyaPedapati@users.noreply.github.com>
2024-09-25 22:43:11 +0530

    Add files via upload

---
+++ import qrcode.py
@@ -0,0 +1,8 @@
+import qrcode
+from PIL import Image
+input_URL="https://www.google.com/"
+qr=qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=15,border=4)
+qr.add_data(input_URL)
+qr.make(fit=True)
+img=qr.make_image(fill_color="red", back_color="white")
+img.show()
